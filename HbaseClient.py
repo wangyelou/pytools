24f865b9f57ddf450ebfcfd228973d8a0c8ee4b5
wangyelou <wangyelou@yeah.net>
2019-06-06 16:12:21 +0800

    优化

--- HbaseClient.py
+++ HbaseClient.py
@@ -6,51 +6,56 @@ from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from hbase import Hbase
 from hbase.ttypes import *
-import traceback 
+import traceback
 import time
 
 
 class HbaseClient():
-
     rety = 1
 
-    def __init__(self, host, **kwargs):
-        self.host = host if host != '' else '127.0.0.1'
-        self.port = int(kwargs.get('port')) if kwargs.get('port') is not None else 9090
-        self.timeout = int(kwargs.get('timeout')) * 1000 if kwargs.get('timeout') is not None else 30000
+    def __init__(self, nodes, timeout = 30000):
+        self.nodes = nodes
+        self.timeout = timeout
         self.__connect()
-        
-        
+
     def __connect(self):
         """
         hbase 连接
         """
-        for i in range(0, self.rety):
+        for index in range(0, len(self.nodes)):
             try:
-                transport = TSocket.TSocket(self.host, self.port)
-                transport.setTimeout(self.timeout)
-                self.transport = TTransport.TBufferedTransport(transport)
-                self.client = Hbase.Client(
-                    TBinaryProtocol.TBinaryProtocol(self.transport)
-                )
-                self.transport.open()
+                host, port = self.nodes[index].split(':')
+                for i in range(0, self.rety):
+                    try:
+                        transport = TSocket.TSocket(host, port)
+                        transport.setTimeout(self.timeout)
+                        self.transport = TTransport.TBufferedTransport(transport)
+                        self.client = Hbase.Client(
+                            TBinaryProtocol.TBinaryProtocol(self.transport)
+                        )
+                        self.transport.open()
+                        break
+                    except:
+                        if i + 1 >= self.rety:
+                            raise Exception('cannot connect hbase, info: %s' % traceback.format_exc())
+
                 break
-            except:
-                if i+1 >= self.rety:
-                    raise Exception('cannot connect hbase, info: %s' % traceback.format_exc())
-    
+            except Exception as e:
+                if index >= len(self.nodes):
+                    raise Exception(e.message)
+
+
     def close(self):
         """
         连接关闭
         """
-        if self.transport:
+        if self.transport.isOpen():
             self.transport.close()
-    
+
     def __reconnect(self):
         self.close()
         self.__connect()
-        
-        
+
     def createTable(self, tableName, columnsFamily):
         """
         创建表
@@ -66,67 +71,71 @@ class HbaseClient():
                     name = item.get("name") if item.get("name") else ""
                     maxVersions = item.get("maxVersions") if item.get("maxVersions") else 3
                     list.append(ColumnDescriptor(name, maxVersions))
-                    
+
                 self.client.createTable(tableName, list)
                 break
             except:
                 self.__reconnect()
-                if i+1 >= self.rety:
+                if i + 1 >= self.rety:
                     raise Exception(traceback.format_exc())
-        
-        
-    def put(self, tableName, row, datas):
+
+    def put(self, tableName, row, timestamp, **kwargs):
         """
         推入数据
-        param tableName
-        param datas
-        [
-            ["col_fam", "c1", "v1"]
-        ]
+        :param tableName
+        :param timestamp
+        :param kwargs BasicInfo = {"col1" : "b1"}
         """
         for i in range(0, self.rety):
             try:
                 mutations = []
-                for item in datas:
-                    if len(item) == 3:
+                for columnFamily, item in kwargs.items():
+                    for column, value in item.items():
                         mutations.append(
                             Mutation(
-                                column = "%s:%s" % (item[0], item[1]),
-                                value = item[2]
+                                column = "%s:%s" % (columnFamily, column),
+                                value = str(value) if value is not None else ''
                             )
                         )
-                
-                self.client.mutateRow(tableName, row, mutations)
-                
+
+                self.client.mutateRowTs(tableName, row, mutations, long(timestamp))
+
                 break
             except:
                 self.__reconnect()
-                if i+1 >= self.rety:
+                if i + 1 >= self.rety:
                     raise Exception(traceback.format_exc())
-        
+
     def getRow(self, tableName, row):
         """
         获取一条数据
         """
         for i in range(0, self.rety):
             try:
-                
+
                 return self.client.getRow(tableName, row)
-                
+
                 break
             except:
                 self.__reconnect()
-                if i+1 >= self.rety:
+                if i + 1 >= self.rety:
                     raise Exception(traceback.format_exc())
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+if __name__ == '__main__':
+
+    client = HbaseClient(
+        ["127.0.0.1:9090",],
+        5000
+    )
+
+    client.put('test', 'row2', 1234564, BasicInfo={"a":"b"})
+
+
+
+
+
+
